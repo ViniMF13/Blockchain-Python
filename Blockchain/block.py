691951b51766bf715f88213aea95301c4a25a59a
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2024-05-21 20:37:04 -0300

    Estrutura Basica Bloco

---
+++ Blockchain/block.py
@@ -0,0 +1,25 @@
+# blockchain/block.py
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, transactions, timestamp=None):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.transactions = transactions
+        self.timestamp = timestamp or time.time()
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.transactions}{self.timestamp}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        target = '0' * difficulty
+        while self.hash[:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
+    def __str__(self):
+        return f"Block(index={self.index}, hash={self.hash}, previous_hash={self.previous_hash}, transactions={self.transactions}, nonce={self.nonce})"
