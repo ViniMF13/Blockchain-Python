8f76cd32cfbb4d13d2567625cf8cd6af90dceef3
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2024-05-21 20:37:25 -0300

    Estrutura básica Blockchain

---
+++ Blockchain/blockchain.py
@@ -0,0 +1,53 @@
+# blockchain/blockchain.py
+from .block import Block
+from .transaction import Transaction
+import time
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 2
+        self.pending_transactions = []
+        self.mining_reward = 50
+
+    def create_genesis_block(self):
+        return Block(0, "0", [], time.time())
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def mine_pending_transactions(self, mining_reward_address):
+        block = Block(len(self.chain), self.get_latest_block().hash, self.pending_transactions)
+        block.mine_block(self.difficulty)
+
+        print("Block successfully mined!")
+        self.chain.append(block)
+
+        self.pending_transactions = [
+            Transaction(None, mining_reward_address, self.mining_reward)
+        ]
+
+    def create_transaction(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    def get_balance_of_address(self, address):
+        balance = 0
+        #itera pelos blocos da cadeia buscando por todas as transações que envolvem o endereço passado 
+        for block in self.chain:
+            for trans in block.transactions:
+                if trans.sender == address:
+                    balance -= trans.amount
+                if trans.receiver == address:
+                    balance += trans.amount
+        return balance
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            if current_block.previous_hash != previous_block.hash:
+                return False
+        return True
