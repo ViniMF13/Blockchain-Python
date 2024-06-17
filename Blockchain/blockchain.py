f82e4de6cffbb3a78649767985a93960c9964cb9
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2024-06-17 14:58:56 -0300

    add database

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -2,6 +2,9 @@
 from .transaction import Transaction
 from .block import Block
 import time
+import os
+import json
+import base64
 
 class Blockchain:
     def __init__(self):
@@ -30,5 +33,22 @@ class Blockchain:
                     balance += transaction.amount
         return balance
 
+    def update_file(self, file_name):
+        with open(file_name, 'w') as file:
+            json.dump([block.to_json() for block in self.chain], file, indent=4)
+        
+    def load_from_file(self, file_name):
+        if not os.path.exists(file_name):
+            return
+        # Verifica se o arquivo está vazio
+        if os.stat(file_name).st_size == 0:
+            return
+        try:
+            with open(file_name, 'r') as file:
+                data = json.load(file)
+                if data:
+                    self.chain = [Block.from_json(block_data) for block_data in data]
+        except FileNotFoundError:
+            pass
 
     
--- Blockchain/transaction.py
+++ Blockchain/transaction.py
@@ -1,13 +1,33 @@
 import hashlib
+import base64
 
 class Transaction:
-    def __init__(self, senderPublicKey, sender, receiver, amount):
+    def __init__(self, senderPublicKey, sender, receiver, amount, signature=None):
         self.senderPublicKey = senderPublicKey
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
-        self.signature = None
+        self.signature = signature
 
+    def to_json(self):
+        return {
+            'senderPublicKey': base64.b64encode(self.senderPublicKey).decode('utf-8') if self.senderPublicKey else None,
+            'sender': self.sender,
+            'receiver': self.receiver,
+            'amount': self.amount,
+            'signature': self.signature
+        }
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(
+            senderPublicKey=base64.b64decode(data['senderPublicKey']) if data['senderPublicKey'] else None,
+            sender=data['sender'],
+            receiver=data['receiver'],
+            amount=data['amount'],
+            signature=data['signature']
+        )
+    
     def calculate_hash(self):
         transaction_data = f'{self.sender}{self.receiver}{self.amount}'
         return hashlib.sha256(transaction_data.encode()).hexdigest()
